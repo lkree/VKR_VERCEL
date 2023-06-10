dc53b37b69589feeb7c11fa920915177e30cf01b
ll.dugenec <xtendmix@gmail.com>
2023-06-10 19:02:30 +0300

    vercel fixes

--- build/entities/File/const/const.js
+++ build/entities/File/const/const.js
@@ -1,6 +1,6 @@
 import { computePathWithDomain } from '../../../shared/api/index.js';
 import { computeDirName } from '../../../shared/lib/helpers/index.js';
-export const FILE_DIRECTORY = computeDirName('/assets/1c/');
+export const FILE_DIRECTORY = computeDirName('/frontend/assets/');
 export const FILE_DEFAULT_NAME = '/leftovers.html';
 export const FRONTEND_FILE_NAME = 'file';
 const computePath = computePathWithDomain('file');
